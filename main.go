1c384e315ad4c4323c4e9a2666cbbf75281d3638
Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>
2020-04-10 18:48:51 +0200

    Improve responses

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -28,49 +30,78 @@ var people []Person
 
 // GetPeopleEndpoint Get all people
 func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(people)
 }
 
 // GetPersonEndpoint Get a person
 func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
-	id, _ := strconv.Atoi(params["id"])
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		fmt.Fprintf(w, "Insert a valid ID")
+		return
+	}
+
 	var res Person
 
 	for _, item := range people {
 		if item.ID == id {
 			res = item
-			break
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(res)
+			return
 		}
 	}
 
-	json.NewEncoder(w).Encode(res)
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "No items were found with that ID")
 }
 
 // AddPersonEndpoint Add a new person
 func AddPersonEndpoint(w http.ResponseWriter, req *http.Request) {
 	var person Person
 	json.NewDecoder(req.Body).Decode(&person)
+	reqBody, err := ioutil.ReadAll(req.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, "Insert a valid data")
+		return
+	}
+	json.Unmarshal(reqBody, &person)
+
 	person.ID = len(people) + 1
 
 	people = append(people, person)
 
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(person)
 }
 
 // DeletePersonEndpoint Delete a person
 func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
-	id, _ := strconv.Atoi(params["id"])
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		fmt.Fprintf(w, "Insert a valid ID")
+		return
+	}
 
 	for index, item := range people {
 		if item.ID == id {
 			people = append(people[:index], people[index+1:]...)
-			break
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(people)
+			return
 		}
 	}
 
-	json.NewEncoder(w).Encode(people)
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "No items were found with that ID")
+
 }
 
 func main() {
@@ -82,8 +113,8 @@ func main() {
 
 	// Endpoints
 	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
 	router.HandleFunc("/people", AddPersonEndpoint).Methods("POST")
+	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":4000", router))
